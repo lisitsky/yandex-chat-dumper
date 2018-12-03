bf5f82a565d164525498eb87d2ef90a3dd2a7a39
Eugene Lisitsky <lisitsky@gmail.com>
2018-12-03 13:51:32 +0300

    Add request URI to logs Output OK after success

--- main.go
+++ main.go
@@ -5,17 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
-
 )
 
-func main()  {
+func main() {
 	log.SetFlags(log.Lshortfile | log.Lmicroseconds | log.Ldate)
 
 	var port = os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	var listenOn = ":"+port
+	var listenOn = ":" + port
 
 	http.HandleFunc("/", handler)
 	log.Printf("Listening on %v", listenOn)
@@ -23,11 +22,12 @@ func main()  {
 	log.Fatalf("Server exited with %v", err)
 }
 
-func handler(w http.ResponseWriter, r *http.Request)  {
+func handler(w http.ResponseWriter, r *http.Request) {
 	var body, err = ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Cannot read body: %v", err)
 		return
 	}
-	log.Printf("Got body `%s` in request %+v ", body, r)
+	log.Printf("Got body `%s` at %v in request %+v ", body, r.RequestURI, r)
+	w.Write([]byte("OK"))
 }
